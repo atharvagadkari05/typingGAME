7fce2dfcd86997400be20579db74f9ffcf32666d
atharvagadkari05 <atharvagadkari0512@gmail.com>
2021-04-29 17:17:50 +0530

    issue resolved

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function startMatch() {
 
 
 function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
+  if (wordInput.value == currentWord.innerHTML) {
     message.innerHTML = 'Correct!!!';
     message.style.color = 'green';
     return true;
